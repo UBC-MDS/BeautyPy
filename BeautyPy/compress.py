b5c93046945de315b47587234f9c76be98218c83
Gilbert Lei <junxiong.lei@gmail.com>
2019-02-08 20:20:39 -0800

    updated proposal, added license file, updated test_emboss.R

--- BeautyPy/compress.py
+++ BeautyPy/compress.py
@@ -5,6 +5,9 @@ import numpy as np
 import matplotlib.image as mpimg
 
 def compress(input_path, output_path):
+    """
+
+    """
     input_img = mpimg.imread(input_path)
 
     mpimg.imsave(output_path, output_img)
