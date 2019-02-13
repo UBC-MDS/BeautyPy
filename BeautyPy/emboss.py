077b14482c0b483097a4259d5e477bef08e6aa20
Gilbert Lei <junxiong.lei@gmail.com>
2019-02-12 23:26:28 -0800

    added implementation of emboss function

--- BeautyPy/emboss.py
+++ BeautyPy/emboss.py
@@ -1,24 +1,101 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+##################################################
+## Project: emboss.py
+## Date: Feb 12th, 2019
+## Author: Gilbert Lei, Jielin Yu, Olivia Lin
+## Purpose: implement the emboss function 
+##################################################
+
 import numpy as np
 import matplotlib.image as mpimg
 
 def emboss(input_path, output_path):
 
     '''
-    This function embosses the original image and saves the embossed image to path.
+    This function embosses the original image and saves the embossed image to output_path.
 
     Parameters
     ---------------------------------------
-    input_path -> the file path for the original image we want to compress
-    output_path ->  the file path to save the compressed image
+    input_path -> the file path for the original image we want to emboss
+    output_path ->  the file path to save the embossed image
 
     Return
     ---------------------------------------
     NA
     '''
     
-    input_img = mpimg.imread(input_path)
+    try:
+        # read input image 
+        input_img = mpimg.imread(input_path)
+    except FileNotFoundError:
+        print("The input path/file does not exist, or the file is not a valid image file.") 
+        raise
+    except TypeError:
+        print("Please provide a string as the path for the input image file.")
+        raise
+    except Exception as e:
+        print("General Error:")
+        print(e)
+        raise
 
-    return
+    # read image dimension 
+    h = len(input_img)
+    w = len(input_img[0])
+    d = len(input_img[0][0])
+
+    # initialize emboss filter 
+    emboss_filter = np.array([[1, 0, 0],
+                              [0, 0, 0],
+                              [0, 0, -1]])
+    
+    # initialize output image array  
+    output_img = np.ones((h-2, w-2, d), dtype="uint8")
+
+    # transform the original red/green/blue color of each pixel into new grayscale, embossed value 
+    for i in range(1, h-2):
+        for j in range(1, w-2): 
+            rgb = [] 
+            for k in range (0, 3): 
+                tl = input_img[i+1][j-1][k]
+                ml = input_img[i][j-1][k]
+                bl = input_img[i-1][j-1][k]
+                tm = input_img[i+1][j][k]
+                mm = input_img[i][j][k]
+                bm = input_img[i-1][j][k]
+                tr = input_img[i+1][j+1][k]
+                mr = input_img[i][j+1][k]
+                br = input_img[i-1][j+1][k]
+
+                neighbors = [[tl, tm, tr],
+                             [ml, mm, mr],
+                             [tr, mr, br]]
+
+                # transform each pixel's r/g/b value using `emboss_filter` 
+                rgb.append(np.sum(np.dot(neighbors, emboss_filter)) + 128)
+            
+            # based on transformed r/g/b values, use below grayscale equation to get new color for each pixel 
+            newcolor = 0.3*rgb[0] + 0.59*rgb[1] + 0.11*rgb[2]
+            
+            # store new color into output array 
+            output_img[i-1][j-1][0] = min(255, max(0, newcolor))
+            output_img[i-1][j-1][1] = min(255, max(0, newcolor))
+            output_img[i-1][j-1][2] = min(255, max(0, newcolor))
+
+    try: 
+        # save output array as an image file
+        mpimg.imsave(output_path, output_img) 
+    except FileNotFoundError:
+        print("The output path does not exist.")
+        raise 
+    except ValueError:
+        print("Please provide a valid file path or valid file type in the output path.")
+        raise
+    except TypeError:
+        print("Please provide a string as the path for the output image file.")
+        raise
+    except Exception as e:
+        print("General Error:")
+        print(e)
+        raise
--- tests/test_emboss.py
+++ tests/test_emboss.py
@@ -1,73 +1,67 @@
-#! /usr/bin/env Rscript
+#! /usr/bin/env python
 
 ##################################################
-## Project: test_emboss.R
+## Project: test_emboss.py
 ## Date: Feb 8th, 2019
 ## Author: Gilbert Lei, Jielin Yu, Olivia Lin
-## Script purpose: This scripts is an unit test for the emboss.R function
+## Purpose: This scripts is an unit test for the emboss.py function
 ##
-## Example: Rscript tests/testthat/test_emboss.R
+## Example: python tests/test_emboss.py
 ##################################################
 
 import numpy as np
 import pytest
 import matplotlib.image as mpimg
+from BeautyPy.emboss import emboss 
 
 # initiate file paths
 test_input_file_path = "test_imgs/emboss/test_input.jpg"
 test_output_file_path = "test_imgs/emboss/test_output.jpg"
-# exp_output_file_path = "test_imgs/emboss/exp_output.jpg"
 
 # prepare test input image array
-test_input = np.array([[[199, 160, 155], [199, 158, 152], [201, 158, 152], [202, 157, 152], [198, 159, 154]],
-                      [[202, 167, 163], [202, 165, 159], [200, 163, 157], [199, 160, 153], [200, 161, 156]],
-                      [[205, 174, 169], [202, 171, 166], [200, 167, 160], [198, 165, 158], [198, 159, 154]],
-                      [[199, 171, 167], [195, 166, 160], [191, 162, 156], [190, 160, 152], [196, 155, 151]],
-                      [[200, 159, 155], [197, 156, 150], [196, 155, 149], [196, 155, 149], [194, 155, 150]],
-                      [[197, 156, 150], [196, 155, 149], [196, 155, 149], [196, 155, 149], [194, 155, 150]],
-                      [[194, 153, 147], [194, 153, 147], [195, 154, 148], [196, 155, 149], [196, 155, 149]],
-                      [[193, 150, 144], [195, 152, 145], [197, 154, 147], [197, 154, 147], [196, 155, 149]],
-                      [[193, 150, 143], [195, 152, 145], [197, 154, 147], [198, 155, 148], [196, 155, 149]],
-                      [[194, 151, 142], [196, 153, 144], [197, 154, 145], [198, 155, 146], [195, 155, 147]],
-                      [[195, 151, 142], [197, 153, 144], [198, 155, 146], [198, 155, 146], [197, 154, 147]]],
+test_input = np.array([[[199,160,155],[199,158,152],[201,158,152],[202,157,152],[198,159,154],[199,160,155]],
+                       [[202,167,163],[202,165,159],[200,163,157],[199,160,153],[200,161,156],[200,161,156]],
+                       [[205,174,169],[202,171,166],[200,167,160],[198,165,158],[198,159,154],[198,159,154]],
+                       [[199,171,167],[195,166,160],[191,162,156],[190,160,152],[196,155,151],[196,155,151]],
+                       [[200,159,155],[197,156,150],[196,155,149],[196,155,149],[194,155,150],[195,156,151]],
+                       [[197,156,150],[196,155,149],[196,155,149],[196,155,149],[194,155,150],[196,157,152]]],
                      dtype="uint8")
 
 # save test input image array as a file on computer
 mpimg.imsave(test_input_file_path, test_input)
 
 # prepare expected output image array
-exp_output = np.array([[[18,18,18], [12,12,12], [1, 1, 1]],
-                       [[12,12,12], [9, 9, 9], [1, 1, 1]],
-                       [[4, 4, 4], [4, 4, 4], [1, 1, 1]],
-                       [[2, 2, 2], [2, 2, 2], [1, 1, 1]],
-                       [[0, 0, 0], [255, 255, 255], [1, 1, 1]],
-                       [[250, 250, 250], [249, 249, 249], [1, 1, 1]],
-                       [[248, 248, 248], [248, 248, 248], [1, 1, 1]],
-                       [[250, 250, 250], [251, 251, 251], [1, 1, 1]],
-                       [[1, 1, 1], [1, 1, 1], [1, 1, 1]]],
+exp_output = np.array([[[144,144,144],[141,141,141],[135,135,135],[1,1,1]],
+                       [[143,143,143],[138,138,138],[128,128,128],[3,3,3]],
+                       [[129,129,129],[127,127,127],[135,135,135],[3,3,3]],
+                       [[1,1,1],[4,4,4],[0,0,0],[0,0,0]]],
                       dtype="uint8")
 
-def test_wrong_file_type():
-    with pytest.raises(TypeError):
-        emboss("test_imgs/emboss/input.pdf", test_output_file_path)
-
-def test_non_string_input():
-    with pytest.raises(AttributeError):
-        emboss(989999999999, test_output_file_path)
-
-def test_non_string_output():
-    with pytest.raises(AttributeError):
-        emboss(test_input_file_path, 2384957372628)
-
-def test_nonexistent_input_path():
+def test_input_path_nonexistent():
     with pytest.raises(FileNotFoundError):
         emboss("test_imgs/hello/world.jpg", test_output_file_path)
 
-def test_nonexistent_output_path():
+def test_input_file_type_wrong():
+    with pytest.raises(FileNotFoundError):
+        emboss("test_imgs/emboss/input.pdf", test_output_file_path)
+
+def test_non_string_input():
+    with pytest.raises(TypeError):
+        emboss(9899999999, test_output_file_path)
+
+def test_output_path_nonexistent():
     with pytest.raises(FileNotFoundError):
         emboss(test_input_file_path, "test_imgs/hello/world.jpg")
+
+def test_output_file_type_wrong():
+    with pytest.raises(ValueError):
+        emboss(test_input_file_path, "test_imgs/emboss/output.ppt")
+
+def test_non_string_output():
+    with pytest.raises(ValueError):
+        emboss(test_input_file_path, 2384957372628)
 
 def test_emboss():
     emboss(test_input_file_path, test_output_file_path)
     test_output = mpimg.imread(test_output_file_path)
-    assert np.array_equal(test_output, exp_output), "The emboss function does not work properly."
+    assert np.array_equal(test_output, exp_output), "The emboss function does not work properly." 
