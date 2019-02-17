e6ea6832e8b4461f9911badb9f5f950e661f12b7
yujielin <jielinyu1997@gmail.com>
2019-02-17 00:16:29 -0800

    update test_flip

--- tests/test_flip.py
+++ tests/test_flip.py
@@ -7,16 +7,26 @@
 import numpy as np
 import skimage.io
 import pytest
-import matplotlib.image as mpimg
 from PIL import Image
 from BeautyPy.flip import flip
 
 # In[13]:
 
 
-test_input_file_path = "test_imgs/compress/test_input.png"
-test_output_file_path = "test_imgs/compress/test_output.png"
+test_input_file_path = "tests/test_imgs/flip/test_input.png"
+test_output_file_path = "tests/test_imgs/flip/test_output.png"
 
+test_input = np.array([[[199,160,155],[199,158,152],[201,158,152],[202,157,152],[198,159,154],[199,160,155]],
+                       [[202,167,163],[202,165,159],[200,163,157],[199,160,153],[200,161,156],[200,161,156]],
+                       [[205,174,169],[202,171,166],[200,167,160],[198,165,158],[198,159,154],[198,159,154]],
+                       [[199,171,167],[195,166,160],[191,162,156],[190,160,152],[196,155,151],[196,155,151]],
+                       [[200,159,155],[197,156,150],[196,155,149],[196,155,149],[194,155,150],[195,156,151]],
+                       [[197,156,150],[196,155,149],[196,155,149],[196,155,149],[194,155,150],[196,157,152]]],
+                     dtype="uint8")
+
+# save test input image array as a PNG file on computer
+inputImage = Image.fromarray(test_input)
+inputImage.save(test_input_file_path)
 
 
 
@@ -25,7 +35,7 @@ test_output_file_path = "test_imgs/compress/test_output.png"
 def test_flip_same_size():
     '''
 
-    Test that the output matrix is correct
+    Test that the output size is correct
 
     '''
     flip(test_input_file_path,test_output_file_path,"v")
@@ -40,11 +50,10 @@ def test_flip_input_type():
 
 
     '''
-    with pytest.raises(TypeError):
-        flip(image,test_output_file_path,"v")
-    with pytest.raises(TypeError):
+
+    with pytest.raises(OSError):
         flip("test_imgs/emboss/input.pdf",test_output_file_path,"v")
-    with pytest.raises(TypeError):
+    with pytest.raises(OSError):
         flip("test_input.jpg",test_output_file_path,"v")
 
 def test_flip_input_shape():
@@ -84,8 +93,8 @@ def test_invalid_input():
     Test if the input path is valid
 
     '''
-    with pytest.raises(AttributeError):
-        flip(test_input_file_path,test_output_file_path,123)
+    with pytest.raises(AssertionError):
+        flip(test_input_file_path,test_output_file_path,"123")
 
 # Check flip vertically middle column
 def test_mid_column_vertical():
@@ -111,7 +120,7 @@ def test_mid_column_vertical():
 
     if n % 2 == 0:
         mid = n//2
-        assert np.array_equal(input_img[mid,:,:],test_output[mid+1,:,:]),"The middle row is not the same"
+        assert np.array_equal(input_img[mid,:,:],test_output[mid-1,:,:]),"The middle row is not the same"
     else:
         mid=n//2
         assert np.array_equal(input_img[mid,:,:],test_output[mid,:,:]),"The middle row is not the same"
